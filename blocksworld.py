ef455a2414ebca2cf16a5537db6d38e06c228cad
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2023-10-23 00:52:10 +0100

    Trying to solve strips exercises

--- blocksworld.py
+++ blocksworld.py
@@ -19,21 +19,40 @@ class Floor(Predicate):
     def __init__(self,block):
         self.args = [block]
 
+    def __hash__(self):
+        return hash((self.args[0]))
+
 class On(Predicate):
     def __init__(self,b1,b2):
         self.args = [b1,b2]
 
+    def __hash__(self):
+        return hash((self.args[0]))
+
 class Free(Predicate):
     def __init__(self,block):
         self.args = [block]
+
+    def __hash__(self):
+        return hash((self.args[0]))
 
 class Holds(Predicate):
     def __init__(self,block):
         self.args = [block]
 
 class HandFree(Predicate):
+    hash_int = 0
+
+    @staticmethod
+    def get_hash_int():
+        HandFree.hash_int += 1
+        return HandFree.hash_int
+
     def __init__(self):
         self.args = []
+
+    def __hash__(self):
+        return hash(self.args[0] if len(self.args) > 0 else 0)
 
 
 # Blocks world operators
--- strips.py
+++ strips.py
@@ -22,18 +22,21 @@ class Predicate:
     def __str__(self):
         argsstr = args2string(self.args)
         return type(self).__name__ + "(" + argsstr + ")"
+
     def __repr__(self):
         return str(self)
-    def __eq__(self,predicate):   # allows for comparisons with "==", etc.
-        return str(self)==str(predicate)
-    def substitute(self,assign): # Substitute the arguments in a predicate
-        la = self.args          # by constants according to a given 
-        if len(la)==0:         # assignment (i.e. a dictionary)
+
+    def __eq__(self, predicate):  # allows for comparisons with "==", etc.
+        return str(self) == str(predicate)
+
+    def substitute(self, assign):  # Substitute the arguments in a predicate
+        la = self.args  # by constants according to a given
+        if len(la) == 0:  # assignment (i.e. a dictionary)
             return type(self)()
-        if len(la)==1:
+        if len(la) == 1:
             return type(self)(assign[la[0]])
         # add other cases if needed
-        return type(self)(assign[la[0]],assign[la[1]])
+        return type(self)(assign[la[0]], assign[la[1]])
 
 
 # STRIPS operators
@@ -41,15 +44,17 @@ class Predicate:
 # -- concrete actions will be instances of specific operators
 class Operator:
 
-    def __init__(self,args,pc,neg,pos):
+    def __init__(self, args, pc, neg, pos):
         self.args = args
-        self.pc  = pc
+        self.pc = pc
         self.neg = neg
         self.pos = pos
+
     def __str__(self):
-        return type(self).__name__ + '([' + args2string(self.args) + "]," +  \
-               str(self.pc) + ',' + str(self.neg) + ',' +  \
-               str(self.pos) + ')'
+        return type(self).__name__ + '([' + args2string(self.args) + "]," + \
+            str(self.pc) + ',' + str(self.neg) + ',' + \
+            str(self.pos) + ')'
+
     def __repr__(self):
         argsstr = args2string(self.args)
         return type(self).__name__ + "(" + argsstr + ")"
@@ -59,14 +64,14 @@ class Operator:
     # called) for the arguments given in "args"
     # ( returns None if the action is not applicable in the given "state" )
     @classmethod
-    def instanciate(cls,args):
-        if len(args)!=len(cls.args):
+    def instanciate(cls, args):
+        if len(args) != len(cls.args):
             return None
         assign = dict(zip(cls.args, args))
-        pc  = [ p.substitute(assign) for p in cls.pc ]
-        neg = [ p.substitute(assign) for p in cls.neg ]
-        pos = [ p.substitute(assign) for p in cls.pos ]
-        return cls(args,pc,neg,pos)
+        pc = [p.substitute(assign) for p in cls.pc]
+        neg = [p.substitute(assign) for p in cls.neg]
+        pos = [p.substitute(assign) for p in cls.pos]
+        return cls(args, pc, neg, pos)
 
 
 # Search domains based on STRIPS actions
@@ -82,7 +87,7 @@ class STRIPS(SearchDomain):
         operators = Operator.__subclasses__()
         actions = []
         for op in operators:
-            lassign = assignments(op.args,constants)
+            lassign = assignments(op.args, constants)
             for assign in lassign:
                 argvalues = [assign[a] for a in op.args]
                 action = op.instanciate(argvalues)
@@ -93,7 +98,16 @@ class STRIPS(SearchDomain):
     # Result of a given "action" in a given "state"
     # ( returns None, if the action is not applicable in the state)
     def result(self, state, action):
-        pass
+        # validar condições
+        for p in action.pc:
+            if p not in state:
+                return None
+
+        # Efeitos negativos
+        newstate = [s for s in state if s not in action.neg]
+
+        # Efeitos positivos
+        return newstate + action.pos
 
     def cost(self, state, action):
         return 1
@@ -103,22 +117,22 @@ class STRIPS(SearchDomain):
 
     # Checks if a given "goal" is satisfied in a given "state"
     def satisfies(self, state, goal):
-        pass
-
+        return all(c in state for c in goal)
 
 
 # Auxiliary functions
 
 def state_constants(state):
-    return list(set(reduce(lambda r,h : h.args+r, state,[])))
+    return list(set(reduce(lambda r, h: h.args + r, state, [])))
 
-def assignments(lvars,lconsts):
-    lcombs = product(lconsts,repeat=len(lvars))
-    makeassign = lambda comb : dict(zip(lvars,comb))
-    return list(map(makeassign,lcombs))
+
+def assignments(lvars, lconsts):
+    lcombs = product(lconsts, repeat=len(lvars))
+    makeassign = lambda comb: dict(zip(lvars, comb))
+    return list(map(makeassign, lcombs))
+
 
 def args2string(args):
     if args == []:
         return ""
-    return reduce(lambda r,h : r+','+str(h), args[1:],str(args[0]))
-
+    return reduce(lambda r, h: r + ',' + str(h), args[1:], str(args[0]))
